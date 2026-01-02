5b173b62adb856e8c6049501ca25ffdc6fe3548f
vidyawadhai13 <vidyawadhai.cse24@sbjit.edu.in>
2026-01-02 18:23:35 +0530

    Add files via upload

---
+++ MyProgram1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class MyProgram1 {
+    public static void main(String[] args) {
+        float area;
+        Rectangle1 r1 = new Rectangle1(); 
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("enter height:");
+        r1.height = sc.nextFloat();
+        
+        System.out.print("enter width:");
+        r1.width = sc.nextFloat();
+        
+        area = r1.calArea();
+        System.out.println("Area=" + area);
+        sc.close(); 
+    }
+}
+
+class Rectangle1 {
+    float height;
+    float width;
+    
+    public float calArea() {
+        return height * width;
+    }
+}
