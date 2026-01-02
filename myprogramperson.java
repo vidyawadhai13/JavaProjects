3d5ec776e91891088ae1982a872d140b91225a93
vidyawadhai13 <vidyawadhai.cse24@sbjit.edu.in>
2026-01-02 10:51:03 +0530

    Add files via upload

---
+++ myprogramperson.java
@@ -0,0 +1,19 @@
+class Person
+{
+String name;
+int age;
+}
+class myprogramperson {
+public static void main(String[] args) {
+Person p1=new Person();
+Person p2=new Person();
+p1.name="modi";
+p1.age=71;
+p2.name="bachchan";
+p2.age=80;
+System.out.println("p1.name="+p1.name);
+System.out.println("p2.name="+p2.name);
+System.out.println("p1.age="+p1.age);
+System.out.println("p2.age="+p2.age);
+}
+}
